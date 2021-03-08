676be14d4fb49286eda4bc461bb2b1e56503dab3
Lathas0123 <lathas2020ls@gmail.com>
2021-03-07 21:29:05 -0800

    first

---
+++ library code.java
@@ -0,0 +1,31 @@
+package library;
+import static java.lang.System.exit;
+import java.util.Scanner;
+public class Library
+{
+
+    String[] username=new String[20];
+    String[] password=new String[20];
+    
+    String[] books=new String[20];
+    
+    String X[] =new String[10];
+    String x;
+    String X1[] =new String[10];
+    String x1;
+     
+     String[] issuebooks=new String[20];
+     String[] Idate=new String[20];
+     String[] Rdate=new String[20];
+    
+    int P[] =new int[10];
+    int p;
+    int no,u=0,u1=0;
+    int code[] ={1,2,3,4,5,6,7,8};
+     
+     int n,g,q=0,k;
+      int i=0,w,l;
+      String issue,ret;
+     String name ,a,b;
+    boolean t;
+    Scanner d=new Scanner(System.in);
